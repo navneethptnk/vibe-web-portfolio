99156b48e396ce3f3f6dbf63ed9c7552f9422cb0
Navneeth patnaik <navneethptnk123@gmail.com>
2026-01-06 12:10:11 +0530

    Add Home component with main sections

---
+++ business-website/src/pages/index.jsx
@@ -0,0 +1,17 @@
+import Hero from "../components/Hero";
+import Services from "../components/Services";
+import WhyUs from "../components/WhyUs";
+import Testimonials from "../components/Testimonials";
+import CTA from "../components/CTA";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <WhyUs />
+      <Testimonials />
+      <CTA />
+    </>
+  );
+}
